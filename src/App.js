e538f7224c4b9aa6afcb8ec213eacac1372de59c
Richa Avasthi <ravasthi@mac.com>
2017-06-10 20:03:28 -0700

    Add eslint, stylelint, form styles.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import logo from './logo.svg';
+import React, { Component } from 'react';
 
 class App extends Component {
   render() {
