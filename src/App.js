b44cdad538b8c65b733e40cc10f8740db6d6cb0e
Richa Avasthi <ravasthi@mac.com>
2017-06-11 13:56:35 -0700

    WIP for adding routing
    This time:
    
    * Add primary nav
    * Adjust page layout to accommodate it
    
    Next up:
    
    * Add React router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import './app.css';
 
-import logo from './logo.svg';
 import React, { Component } from 'react';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <div className="app-header">
-          <img src={logo} className="app-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="page">
+        <div className="primary-nav-container">
+          <nav className="primary">
+            <ul>
+              <li>Page one</li>
+              <li>Page two</li>
+              <li>Page three</li>
+            </ul>
+          </nav>
         </div>
-        <p className="app-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="page-content-container">
+          <div className="page-content">
+            <div className="app-header">
+              <h1>Yet another React app starter</h1>
+            </div>
+            <p className="app-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
