dcde7694a587d92349b41e89f0c8d09394ead207
Richa Avasthi <ravasthi@mac.com>
2017-06-21 01:10:52 -0700

    Add basic tests for FontWeightTester.

--- src/components/FontWeightTester.test.js
+++ src/components/FontWeightTester.test.js
@@ -8,18 +8,52 @@ import FontWeightTester from 'components/FontWeightTester';
 import React from 'react';
 
 describe('FontWeightTester', () => {
+  let component;
+
   before(() => {
     initTests();
+  });
+
+  beforeEach(() => {
+    component = mount(<FontWeightTester />);
+  });
+
+  afterEach(() => {
+    component.unmount();
   });
 
   after(() => {
     cleanUpTests();
   });
 
-  it('renders properly', () => {
-    const app = mount(<FontWeightTester />);
+  it('should render properly with default props', () => {
+    expect(component.find('form.choose-family')).to.have.length(1);
+    expect(component.find('input[type="text"].family')).to.have.length(1);
 
-    // expect(app.find('nav.primary')).to.have.length(1);
+    expect(component.state('family')).to.equal('Avenir Next');
+    expect(component.find('.family-under-test')).to.have.text().equal('Avenir Next');
+    expect(component.find('.displayed-font')).to.have.style('font-family', 'Avenir Next');
+  });
+
+  it('should render properly with custom props', () => {
+    component = mount(<FontWeightTester family="Helvetica Neue" />);
+
+    expect(component.state('family')).to.equal('Helvetica Neue');
+    expect(component.find('.family-under-test')).to.have.text().equal('Helvetica Neue');
+    expect(component.find('.displayed-font')).to.have.style('font-family', 'Helvetica Neue');
+  });
+
+  describe('interactions', () => {
+    it('should update properly when a new font is chosen', () => {
+      const input = component.find('input.family');
+
+      input.node.value = 'Georgia';
+      component.find('form.choose-family').simulate('submit');
+
+      expect(component.state('family')).to.equal('Georgia');
+      expect(component.find('.family-under-test')).to.have.text().equal('Georgia');
+      expect(component.find('.displayed-font')).to.have.style('font-family', 'Georgia');
+    });
   });
 });
 
