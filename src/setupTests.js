26164c54af6eddc04524b0c6e864a54550852dc6
Richa <Avasthi>
2020-11-04 19:34:12 -0800

    Round 7: react, redux

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,4 @@
 import 'jest-extended';
 // react-testing-library renders your components to document.body,
 // this adds jest-dom's custom assertions
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
